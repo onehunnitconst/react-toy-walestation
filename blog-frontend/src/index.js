00b9994611c2dec583b6f1a8852afaa2421e9c7d
백상수 <skygana@gmail.com>
2020-12-08 01:34:57 +0900

    register 코드 수정

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import rootReducer from './modules/index';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules/index';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
--- blog-frontend/src/lib/createRequestSaga.js
+++ blog-frontend/src/lib/createRequestSaga.js
@@ -1,5 +1,10 @@
 import { call, put } from 'redux-saga/effects';
 import { startLoading, finishLoading } from '../modules/loading';
+export const createRequestActionTypes = type => {
+    const SUCCESS = `${type}_SUCCESS`;
+    const FAILURE = `${type}_FAILURE`;
+    return [type, SUCCESS, FAILURE];
+}
 
 export default function createRequestSaga(type, request) {
     const SUCCESS = `${type}_SUCCESS`;
@@ -7,5 +12,19 @@ export default function createRequestSaga(type, request) {
 
     return function*(action) {
         yield put(startLoading(type));
-    }
+        try {
+            const response = yield call(request, action.payload);
+            yield put({
+                type: SUCCESS,
+                payload: response.data,
+            });
+        } catch (e) {
+            yield put({
+                type: FAILURE,
+                payload: e,
+                error: true,
+            });
+        }
+        yield put(finishLoading(type));
+    };
 }
