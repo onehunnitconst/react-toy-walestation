ef61995f8efa409658f382aebece92b24a730033
백상수 <skygana@gmail.com>
2020-12-21 00:36:26 +0900

    post view 추가

--- blog-frontend/src/pages/PostPage.js
+++ blog-frontend/src/pages/PostPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import PostViewer from '../components/post/PostViewer';
+import HeaderContainer from '../containers/common/HeaderContainer';
+import PostViewerContainer from '../containers/post/PostViewerContainer';
 
 const PostPage = () => {
     return (
-        <div></div>
+        <div>
+            <HeaderContainer />
+            <PostViewerContainer />
+        </div>
     )
 }
 
